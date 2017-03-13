53ba7101bd5410c0c1c41dcef77d39954e2a9bfc
Alex Nguyen <alex.nguyen@td.com>
2017-03-12 21:12:45 -0400

    Initial Commit

---
+++ CloudFunctionExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudFunctionExample
+//
+//  Created by Alex Nguyen on 2017-03-12.
+//  Copyright © 2017 Alex Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
